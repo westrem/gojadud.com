11407936612acac921bfe7d22eb63f601313565d
Jan Jadud <dev@gojadud.com>
2020-03-14 21:47:40 +0100

    Fix deploy task

--- v2/gulp_tasks/deploy.js
+++ v2/gulp_tasks/deploy.js
@@ -4,7 +4,7 @@ import { buildDir } from './../version';
 
 function deploy() {
   console.log('running: Deploy');
-  return gulp.src(PATHS.APP.DEPLOY.src)
+  return gulp.src(PATHS.APP.DEPLOY.src, { dot: true })
     .pipe(gulp.dest(PATHS.APP.DEPLOY.dest + buildDir));
 }
 
